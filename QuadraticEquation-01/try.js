33c745bb2902c4556ac31242495aac61dc313941
Elena Mankova <emankova@gmail.com>
2016-10-11 21:23:29 +0400

    исправила согласно комментариям

--- QuadraticEquation-01/try.js
+++ QuadraticEquation-01/try.js
@@ -1,29 +1,30 @@
-function solution(first, second, third) {
+function solve(first, second, third) {
 	var discr = 1*second**2 - 4*first*third;
 	var sqrtOfDiscr = discr ** (1/2);
 
 	if (discr > 0) {
 		var x1 = ((-1)*second + sqrtOfDiscr)/(2*first);
 		var x2 = ((-1)*second - sqrtOfDiscr)/(2*first);
+		return [x1, x2]
 	}
 	else if (discr === 0) {
 		var x3 = ((-1)*second)/(2*first);
+		return [x3]
 	}
 	else {
-	
-	};
-	return [x1, x2, x3];
+		return 'нет решений'
+			};
 };
 
 var a = parseFloat(window.prompt('чему равен коэффициент а'));
 var b = parseFloat(window.prompt('чему равен коэффициент b'));
 var c = parseFloat(window.prompt('чему равен коэффициент c'));
-var resultList = solution(a, b, c);
-if (resultList[0] === undefined && resultList[1] === undefined) {
-	console.log(resultList[2]);
+var resultList = solve(a, b, c);
+if (resultList.lenght = 2) {
+	console.log(resultList);
 }
-else if (resultList[2] === undefined) {
-	console.log(resultList[0] + ', ' + resultList[1]);
+else if (resultList.lenght = 1) {
+	console.log(resultList);
 }
 else {
 	console.log(resultList);	
